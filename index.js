c2d0f6b33ea0cc7080f2ef1630b7ab96ce153635
Dhananjay shinde <devine@localhost.localdomain>
2020-10-03 18:33:00 +0530

    configure middlewares

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 const express = require("express");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const newsRoute = require("./routes/api/news");
 const app = express();
+const oracledb = require("oracledb");
+const db_config = require("./routes/config/dbConfig");
 
-app.get("/", function (req, res) {
-  res.send("Hello World");
+// security middlewares
+app.use(helmet());
+app.use(morgan("combined"));
+
+//connect to database
+oracledb.createPool(db_config, (err, res) => {
+  if (err) return console.log(err.message);
+  console.log("Connected to DB...");
 });
 
-app.listen(3000, () => console.log(`server running on port 3000.....`));
+app.get("/", function (req, res) {
+  res.json({
+    message: "........Welcome to News API........",
+    requestedAt: new Date(),
+  });
+});
+
+// routes middlewares
+app.use("/api/news", newsRoute);
+
+app.listen(8080, () => console.log(`server running on port 8080.....`));
