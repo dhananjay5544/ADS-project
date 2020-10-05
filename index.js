025e4687dc1e409da5f0f844b74b093c16ac1770
Dhananjay Shinde <dhananjayshinde1999@gmail.com>
2020-10-05 19:18:49 +0530

    add middlewares

--- index.js
+++ index.js
@@ -2,28 +2,41 @@ const express = require("express");
 const helmet = require("helmet");
 const morgan = require("morgan");
 const newsRoute = require("./routes/api/news");
+const middlewares = require("./routes/api/middlewares");
 const app = express();
-const oracledb = require("oracledb");
-const db_config = require("./routes/config/dbConfig");
+
+require("dotenv").config();
 
 // security middlewares
 app.use(helmet());
 app.use(morgan("combined"));
+app.use(express.json());
 
-//connect to database
-oracledb.createPool(db_config, (err, res) => {
-  if (err) return console.log(err.message);
-  console.log("Connected to DB...");
+app.get("/", authenticate, function (req, res) {
+	res.json({
+		message: "✨✨...Welcome to News API...✨✨",
+		requestedAt: new Date(),
+	});
 });
 
-app.get("/", function (req, res) {
-  res.json({
-    message: "........Welcome to News API........",
-    requestedAt: new Date(),
-  });
-});
+function authenticate(req, res, next) {
+	const header = req.headers["authorization"];
+	const apikey = header && header.split(" ")[1];
+	if (apikey !== process.env.API_KEY) {
+		res.status(401).json({
+			message: "🚫Unauthorised Access....",
+			requestedAt: new Date(),
+		});
+	} else {
+		next();
+	}
+}
 
 // routes middlewares
 app.use("/api/news", newsRoute);
 
-app.listen(8080, () => console.log(`server running on port 8080.....`));
+app.use(middlewares.notFound);
+app.use(middlewares.errorHandler);
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}.....`));
